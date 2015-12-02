729fd2ebc928af25016b334e6ae9ca660453fb9f
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2015-12-02 16:58:44 +0100

    adding generator

--- parse.py
+++ parse.py
@@ -2,8 +2,43 @@ import argparse
 from os import path as op
 import os
 
-subsets_names = ['Dev', 'Test']
-sources_names = ['bass', 'drums', 'other', 'vocals']
+
+def get_mds_track(root_dir=None, subsets=['Dev', 'Test']):
+
+    mixtures_folder = op.join(root_dir, "Mixtures")
+    sources_folder = op.join(root_dir, "Sources")
+
+    sources_names = ['bass', 'drums', 'other', 'vocals']
+
+    # parse all the mixtures
+    if op.isdir(mixtures_folder):
+        for subset in subsets:
+            subset_folder = op.join(mixtures_folder, subset)
+            for _, track_folders, _ in os.walk(subset_folder):
+                for track_name in track_folders:
+                    track = {}
+                    track['name'] = track_name
+                    track['base_path'] = op.join(subset_folder, track_name)
+                    track['path'] = op.join(
+                        track['base_path'], 'mixture.wav'
+                    )
+                    track['sources'] = []
+                    for source in sources_names:
+                        track['sources'].append(
+                            {
+                                'name': source,
+                                'path': op.join(
+                                    sources_folder,
+                                    subset,
+                                    track_name,
+                                    source
+                                ) + '.wav'
+                            }
+                        )
+                    yield track
+    else:
+        print "%s not exists." % op.join("Estimates", args.mds_folder)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -12,31 +47,5 @@ if __name__ == '__main__':
     parser.add_argument('mds_folder', type=str, help='MDS 100 Folder')
 
     args = parser.parse_args()
-    mixtures_folder = op.join(args.mds_folder, "Mixtures")
-    sources_folder = op.join(args.mds_folder, "Sources")
 
-    # build a dict
-    tracks = {}
-    # parse all the mixtures
-    if op.isdir(mixtures_folder):
-        for subset in subsets_names:
-            subset_folder = op.join(mixtures_folder, subset)
-            print subset_folder
-            for _, track_folders, _ in os.walk(subset_folder):
-                for track_name in track_folders:
-                    print track_name
-                    track_path = op.join(subset_folder, track_name)
-                    print track_path
-                    track_mixture_path = op.join(track_path, 'mixture.wav')
-                    print track_mixture_path
-                    track_sources_paths = []
-                    for source in sources_names:
-                        track_sources_paths.append(
-                            op.join(
-                                sources_folder, subset, track_name, source
-                            ) + '.wav'
-                        )
-                    print track_sources_paths
-
-    else:
-        print "%s not exists." % op.join("Estimates", args.mds_folder)
+    print list(get_mds_track(args.mds_folder))
