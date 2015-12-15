91c692606f99297f5df75e3f4be8a42dff65cb3d
Nils Werner <nils.werner@audiolabs-erlangen.de>
2015-12-15 14:10:18 +0100

    Fixed conf so it works on readthedocs.org

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,15 @@
 import sys
 import os
 import shlex
-import sphinx_rtd_theme
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+else:
+    html_theme = 'default'
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -32,10 +40,6 @@ exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 
 todo_include_todos = False
-
-html_theme = "sphinx_rtd_theme"
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_static_path = ['_static']
 
