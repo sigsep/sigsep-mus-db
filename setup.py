08bab6f8d941639e732297a2daf9b1b79c44dd43
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2016-01-07 17:51:08 +0100

    version up

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         name='dsd100',
 
         # Version
-        version="0.1.0",
+        version="0.2.0",
 
         # Description
         description='Easily parse the SISEC Demixing Secrets Dataset',
