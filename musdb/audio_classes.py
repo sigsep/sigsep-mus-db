2b9ba2343ce1c22d0ccf5631a09f13fd3e66aa60
Fabian-Robert Stöter <fabian-robert.stoter@inria.fr>
2019-06-25 12:15:38 +0200

    revert subset

--- musdb/audio_classes.py
+++ musdb/audio_classes.py
@@ -18,6 +18,8 @@ class Track(object):
         stem/substream ID
     is_wav : boolean
         If stem is read from wav or mp4 stem
+    subset : {'train', 'test'}
+        Track belongs to which subset.
     targets : OrderedDict
         OrderedDict of mixted Targets for this ``track``.
     sources : Dict
@@ -33,10 +35,12 @@ class Track(object):
         path="None",
         is_wav=False,
         stem_id=None,
+        subset=None,
         chunk_start=0,
         chunk_duration=None
     ):
         self.path = path
+        self.subset = subset
         self.stem_id = stem_id
         self.is_wav = is_wav
 
