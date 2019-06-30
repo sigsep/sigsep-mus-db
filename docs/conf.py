1585d58f550188abd7f3caaf76442d4a460da230
Fabian-Robert Stöter <fabian-robert.stoter@inria.fr>
2019-06-30 11:15:26 +0200

    typo

--- docs/conf.py
+++ docs/conf.py
@@ -136,8 +136,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'musdb.tex', 'musdb Documentation',
-     'Fabian-Robert Stoeter, 'manual'),
+    (master_doc, 'musdb.tex', 'musdb Documentation', 'Fabian-Robert Stoeter', 'manual'),
 ]
 
 
